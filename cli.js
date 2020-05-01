33cd48d1ef28faa42c3c3dbb0997e21c43f61cee
Ran Bar-Zik <ran@bar-zik.com>
2020-05-02 02:26:13 +0300

    adding backward compatability

---
+++ cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const childProcess = require('child_process');
+
+process.emitWarning('branch-name-lint will stop using ./cli.js path in the future. Please use npx branch-name-lint');
+
+childProcess.fork('./bin/branch-name-lint');
